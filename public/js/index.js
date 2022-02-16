146154cd0eca0ec610dbe6ca7aad452b5e1c6ab3
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2022-02-15 23:29:37 -0300

    added express server

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#root").innerHTML = `
+    <h1>Hello world</h1>
+    <h2>Hello world</h2>
+    <h3>Hello world</h3>
+    <p>Hello world</p>
+`;
--- server.js
+++ server.js
@@ -1,70 +1,14 @@
-// http module allows Node.js to use the HTTP and create a Web Server
-const http = require("http");
-// file system module allows the server to work with static files
-const fs = require("fs");
-// path module allows the server to work with directories and file paths
+const express = require("express");
+const app = express();
 const path = require("path");
+const port = 3000;
 
-// MIME-types contains the types of documents accepted by the server
-const mimeTypes = require("./mimeTypes");
-// port receives a environment variable or a specific port for the server listen to
-const port = process.env.PORT || 3000;
-// path to the index file
-const indexPage = "./index.html";
-// path to the static folder
-const staticFolder = "./static";
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-// on creating the server, req is the client request and res is the server response
-http
-	.createServer((req, res) => {
-		// filePath will receive the path of every file transferred by the server at the request
-		let filePath = "." + req.url;
-		// if the file requested by the url is not in the static folder, it's a url route and the content shown will be index.html
-		if (!filePath.includes(staticFolder)) {
-			// on this application, the server will no be responsible for routing, the router.js will
-			// so every url settled by the user will get index.html as response
-			filePath = indexPage;
-		}
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 
-		console.log(filePath);
-		console.log(req.url);
-
-		// path.extname extracts the file extension of filePath
-		let extname = String(path.extname(filePath)).toLowerCase();
-		// contentType receives the extname MIME-type if it is declared in mimeTypes.js or a generic binary MIME-type
-		let contentType = mimeTypes[extname] || "application/octet-stream";
-
-		// fs.readFile tries to read the filePath content
-		fs.readFile(filePath, (error, content) => {
-			// if there is an error on the file path
-			if (error) {
-				filePath = indexPage;
-				let extname = String(path.extname(filePath)).toLowerCase();
-				let contentType = mimeTypes[extname] || "application/octet-stream";
-				fs.readFile(filePath, (error, content) => {
-					res.writeHead(200, { "Content-Type": contentType });
-					res.end(content, "utf-8");
-				});
-				// // and it is an invalid requested url
-				// if (error.code === "ENOENT") {
-
-				// }
-				// // and it is a server problem
-				// else {
-				// 	res.writeHead(500, { "Content-Type": "text/html" });
-				// 	res.end("Server error", "utf-8");
-				// }
-			}
-			// if all goes well, it sends the filePath content to the client
-			else {
-				res.writeHead(200, { "Content-type": contentType });
-				res.end(content, "utf-8");
-			}
-
-			// HTTP response status codes:
-			// 200 >> success
-			// 404 >> not found
-			// 500 >> internal server error
-		});
-	}) // server listening to the right port and logging a message on the console
-	.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
